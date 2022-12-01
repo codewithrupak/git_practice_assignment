fb6f1aad0c7d3d638a3a76cae035382e1a5b01d4
codewithrupak <rupakdash999@gmail.com>
2022-12-02 02:36:01 +0530

    my_part_is_done

---
+++ wrong_code.js
@@ -0,0 +1,30 @@
+//prime problem
+
+let num = 13;
+let count = 0;
+for(x = 0; x < N; x++){
+   if(num % x == 2){
+     count+;
+   }
+  }
+
+ if(count == 0){
+   console.log(num,"is a prime number");
+   
+   console.log(num,"is not a prime number");
+
+
+//palindrome problem
+
+let str = "nrupul"
+let N = ;
+let jhola = "";
+for(x = N-1; x >= 0; x--){
+   jhola += x;
+  }
+  
+if(str == jhola){
+ 
+else{
+  console.log(str, is not a palindrome");
+
