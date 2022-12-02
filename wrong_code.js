aa22ca10bab45830ef7190da774efe8e9c39e23e
ashu7982 <[ashutoshdubey7982@gmail.com]>
2022-12-02 19:59:37 +0530

    code is done now

--- wrong_code.js
+++ wrong_code.js
@@ -2,29 +2,29 @@
 
 let num = 13;
 let count = 0;
-for(x = 0; x < N; x++){
-   if(num % x == 2){
-     count+;
+for(x = 1; x <= num; x++){
+   if(num % x == 0){
+     count++;
    }
   }
 
- if(count == 0){
+ if(count == 2){
    console.log(num,"is a prime number");
-   
+   }else{
    console.log(num,"is not a prime number");
-
+}
 
 //palindrome problem
 
 let str = "nrupul"
-let N = ;
+let N =6 ;
 let jhola = "";
-for(x = N-1; x >= 0; x--){
-   jhola += x;
+for(let i = N-1; i >= 0; i--){
+   jhola += i;
   }
   
 if(str == jhola){
- 
-else{
-  console.log(str, is not a palindrome");
-
+ console.log(str,'it is pallindrome')
+}else{
+  console.log(str,"is not a palindrome");
+}
